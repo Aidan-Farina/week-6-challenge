feb2b4e51dcbfb51fb9504f030d43a74c6d531d6
Aidan <farinaaidan2@gmail.com>
2023-04-24 16:00:32 -0400

    did a lot of work on the html and added bulma api

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+//variables
+var searchButton = $(".btn")
+
+
+//make it so that the search button takes take the name and compares it to a known location
+
+//log searched areas to search history
+
+//show todays weather based on location searched
+
+//show the 5 day forecast based on location searched
+
+});
